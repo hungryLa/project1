196843c1f4a46cdf203fc6d5d4fd398ec8912a10
hungryLa <hungry_la@mail.ru>
2022-12-20 20:20:42 +0500

    Создал шаблон для главной страницы

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('main.index');
+        $products = Product::all();
+        return view('main.index',compact(['products']));
     }
 }
---
+++ resources/views/include/mainHeader.blade.php
@@ -0,0 +1,70 @@
+<nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Курилка №1</a>
+        </div>
+        <div class="collapse navbar-collapse" id="custom-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="index_mp_fullscreen_video_background.html">Default</a></li>
+                        <li><a href="index_op_fullscreen_gradient_overlay.html">One Page</a></li>
+                        <li><a href="index_agency.html">Agency</a></li>
+                        <li><a href="index_portfolio.html">Portfolio</a></li>
+                        <li><a href="index_restaurant.html">Restaurant</a></li>
+                        <li><a href="index_finance.html">Finance</a></li>
+                        <li><a href="index_landing.html">Landing Page</a></li>
+                        <li><a href="index_photography.html">Photography</a></li>
+                        <li><a href="index_shop.html">Shop</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Headers</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Static Image Header</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index_mp_fullscreen_static.html">Fulscreen</a></li>
+                                <li><a href="index_mp_classic_static.html">Classic</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Flexslider Header</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index_mp_fullscreen_flexslider.html">Fulscreen</a></li>
+                                <li><a href="index_mp_classic_flexslider.html">Classic</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Video Background Header</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index_mp_fullscreen_video_background.html">Fulscreen</a></li>
+                                <li><a href="index_mp_classic_video_background.html">Classic</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Text Rotator Header</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index_mp_fullscreen_text_rotator.html">Fulscreen</a></li>
+                                <li><a href="index_mp_classic_text_rotator.html">Classic</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Gradient Overlay Header</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index_mp_fullscreen_gradient_overlay.html">Fulscreen</a></li>
+                                <li><a href="index_mp_classic_gradient_overlay.html">Classic</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Features</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="alerts-and-wells.html"><i class="fa fa-bolt"></i> Alerts and Wells</a></li>
+                        <li><a href="buttons.html"><i class="fa fa-link fa-sm"></i> Buttons</a></li>
+                        <li><a href="tabs_and_accordions.html"><i class="fa fa-tasks"></i> Tabs &amp; Accordions</a></li>
+                        <li><a href="content_box.html"><i class="fa fa-list-alt"></i> Contents Box</a></li>
+                        <li><a href="forms.html"><i class="fa fa-check-square-o"></i> Forms</a></li>
+                        <li><a href="icons.html"><i class="fa fa-star"></i> Icons</a></li>
+                        <li><a href="progress-bars.html"><i class="fa fa-server"></i> Progress Bars</a></li>
+                        <li><a href="typography.html"><i class="fa fa-font"></i> Typography</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
